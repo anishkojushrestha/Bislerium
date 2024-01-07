e14273ef62a781a97e86c98dd81bf28a396b16a2
awneesh shrestha <87461543+anishkojushrestha@users.noreply.github.com>
2024-01-07 23:47:48 +0545

    validation

--- Bislerium/Data/User.cs
+++ Bislerium/Data/User.cs
@@ -6,5 +6,6 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public Role role { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
